d1de6994304da757f51bdf6b88c574aa07cfb18a
Kai Buelk <kai.buelk@gmail.com>
2023-12-19 14:09:13 +0100

    Trying to add new book adding functionality

--- app.js
+++ app.js
@@ -3,8 +3,13 @@ const myLibrary = [];
 const container = document.querySelector('#container');
 const btnAddBook = document.querySelector('#btnAddBook');
 const dialog = document.querySelector('#dialog1');
+const bookForm = document.querySelector('#bookForm');
 
 btnAddBook.addEventListener('click',() => dialog.showModal());
+bookForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    getBookDataAndAdd();
+});
 
 
 function Book(title, author, pages, checkRead) {
@@ -23,37 +28,43 @@ addBookToLibrary(newBook);
 const newBook2 = new Book('Wealth of Nations', 'Adam Smith', 623, false);
 addBookToLibrary(newBook2);
 
-for (let i = 0; i < myLibrary.length; i++) {
-    const bookElement = myLibrary[i];
-    const bookDisplay = document.createElement('div');
-    const checkBox = document.createElement('INPUT');
-    checkBox.setAttribute('class' , 'checkBox')
-    checkBox.setAttribute("type", 'checkbox');
-    bookElement.checkRead == true ? checkBox.checked = true : checkBox.checked = false;
+function displayLibrary() {
+    const element = document.querySelector('.bookDisplay');
+    if (element) {
+        container.removeChild(bookDisplay)
+    }
+    for (let i = 1; i <= myLibrary.length; i++) {
+        const bookElement = myLibrary[i];
+        const bookDisplay = document.createElement('div');
+        bookDisplay.setAttribute('class', 'bookDisplay');
+        const checkBox = document.createElement('INPUT');
+        checkBox.setAttribute('class' , 'checkBox')
+        checkBox.setAttribute("type", 'checkbox');
+        bookElement.checkRead == true ? checkBox.checked = true : checkBox.checked = false;
 
-    bookDisplay.setAttribute('id', i);
-    bookDisplay.setAttribute('class', 'singleBook')
-    bookDisplay.textContent = bookElement.title 
-        + ' ' + bookElement.author
-        + ' ' + bookElement.pages;
-
-    bookDisplay.appendChild(checkBox);
-    container.appendChild(bookDisplay);
+        bookDisplay.setAttribute('id', i);
+        bookDisplay.setAttribute('class', 'singleBook')
+        bookDisplay.textContent = bookElement.title 
+            + ' ' + bookElement.author
+            + ' ' + bookElement.pages;
+    
+        bookDisplay.appendChild(checkBox);
+        container.appendChild(bookDisplay);
+    }
 }
 
-const inputSubmitted = document.querySelector('#submitInputs');
-inputSubmitted.addEventListener('click', () => getBookAndAdd());
 
-
-function getBookAndAdd() {
+function getBookDataAndAdd() {
     const bookNameInput = document.querySelector('#bookNameInput').value;
     const bookAuthorInput = document.querySelector('#bookAuthorInput').value;
     const bookPagesInput = document.querySelector('#bookPagesInput').value;
-    const bookReadInput = document.querySelector('#bookReadInput');
+    let bookReadInput = document.querySelector('#bookReadInput');
     bookReadInput.checked == true ? bookReadInput = true : bookReadInput = false;
     const bookToAdd = new Book(bookNameInput, bookAuthorInput, bookPagesInput, bookReadInput);
     console.log(bookToAdd);
     addBookToLibrary(bookToAdd);
     dialog.close();
+
 }
 
+displayLibrary();
